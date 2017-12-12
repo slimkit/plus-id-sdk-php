c5079757addb70c9208cdb6991c8be36f50f77a8
Seven Du <shiweidu@outlook.com>
2017-12-12 16:24:49 +0800

    增加检查用户接口。

---
+++ src/SlimKit/PlusID/Client/Auth/Resolve.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace SlimKit\PlusID\Client\Auth;
+
+use SlimKit\PlusID\Client\Client;
+use SlimKit\PlusID\Client\Support\URL;
+
+class Resolve
+{
+    protected $client;
+    protected $user;
+    protected $time;
+
+    public function __construct(Client $client)
+    {
+        $this->client;
+        $this->time = time();
+    }
+
+    public function getTime(): int
+    {
+        return $this->time;
+    }
+
+    public function getSign(): string
+    {
+        $action = [
+            'app' => $this->client->getApp(),
+            'action' => 'auth/resolve',
+            'time' => $this->time,
+        ];
+
+        return $this->client->getSign($action);
+    }
+
+    public function getURI(): string
+    {
+        $url = new URL($this->client->getServe());
+        $url->addQuery('time', $this->time);
+        $url->addQuery('sign', $this->getSign());
+        $url->addQuery('action', 'auth/login');
+
+        return $url->make();
+    }
+}
